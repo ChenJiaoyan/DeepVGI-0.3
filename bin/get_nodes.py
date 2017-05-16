ca432549d6df1cd0eb77716a8f9b4add0dcc5d05
yan <zhouyan090@gmail.com>
2017-05-16 23:15:30 +0200

    debug

--- bin/get_nodes.py
+++ bin/get_nodes.py
@@ -42,8 +42,8 @@ def get_nodes(shpfile):
 
 if __name__ == "__main__":
 
-    shapefile = '../data/shp/test_nodesapi.shp'
-    output = '../data/test.csv'
+    shapefile = '../data/shp/select1.shp'
+    output = '../data/select1.csv'
     fields = ['osm_id', 'lat', 'lon']
     csvfile = open(output, 'wb')
     writer = csv.writer(csvfile)
