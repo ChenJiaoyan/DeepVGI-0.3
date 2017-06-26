94fc8873a561a872e38dc66426ec8c7deebcb386
yan <zhouyan090@gmail.com>
2017-06-26 13:45:00 +0200

    fileIO

--- exp1/DL_GPX.py
+++ exp1/DL_GPX.py
@@ -14,12 +14,12 @@ cv_n = 4
 print '--------------- Read Samples ---------------'
 client = OSM.GPXclient()
 train_imgs, test_imgs = client.imgs_cross_validation(cv_i, cv_n)
-osm_p_imgs = client.read_p_images()
-osm_n_imgs = client.read_n_images()
+gpx_p_imgs = client.read_p_images()
+gpx_n_imgs = client.read_n_images()
 print 'train_imgs: %d \n' % len(train_imgs)
-print 'gpx_p_imgs: %d \n' % len(osm_p_imgs)
-print 'gpx_n_imgs: %d\n' % len(osm_n_imgs)
-img_X, Y = DL_OSM.read_train_sample(tr_n1, tr_n0, train_imgs, osm_p_imgs, osm_n_imgs)
+print 'gpx_p_imgs: %d \n' % len(gpx_p_imgs)
+print 'gpx_n_imgs: %d\n' % len(gpx_n_imgs)
+img_X, Y = DL_OSM.read_train_sample(tr_n1, tr_n0, train_imgs, gpx_p_imgs, gpx_n_imgs)
 m = NN_Model.Model(img_X, Y, nn + '_ZY')
 
 if not evaluate_only:
@@ -34,8 +34,8 @@ del img_X, Y, train_imgs
 gc.collect()
 
 print '--------------- Evaluation on GPX Samples ---------------'
-osm_p_imgs = client.read_p_images()
-osm_n_imgs = client.read_n_images()
-img_X2, Y2 = DL_OSM.read_test_sample(te_n, test_imgs, osm_p_imgs, osm_n_imgs)
+gpx_p_imgs = client.read_p_images()
+gpx_n_imgs = client.read_n_images()
+img_X2, Y2 = DL_OSM.read_test_sample(te_n, test_imgs, gpx_p_imgs, gpx_n_imgs)
 m.set_evaluation_input(img_X2, Y2)
 m.evaluate()
